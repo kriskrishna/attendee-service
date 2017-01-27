2fc28886f32b7a9ad73af00c513dfecf1b696a48
System Administrator <root@Kshitizs-MacBook-Pro.local>
2017-01-27 14:32:41 -0700

    fixing

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -4,4 +4,4 @@ set -e +x
 
 cd attendee-service-source
 ./mvnw clean package
-mv attendee-service.jar ../package-output/
+mv attendee-service-0.0.1-SNAPSHOT.jar ../package-output/attendee-service.jar
